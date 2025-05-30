9982546a60900ff58be62a04135378b144be8f25
Westerntf <dgosley22@gmail.com>
2025-05-30 17:46:15 +1000

    Fix getResultLabel display in results summary section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -6,6 +7,9 @@ import Footer from "../components/Footer";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
